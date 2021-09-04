3abe63fbb766dac34fe2154e158431874d6e8c5b
Adaxator <adax324@gmail.com>
2021-09-04 18:01:29 +0200

    initial commit

---
+++ src/test/java/com/drivingschool/DrivingSchool/DrivingSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivingschool.DrivingSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
