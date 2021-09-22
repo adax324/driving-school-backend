6e0e48d08e7664f5a891ef86faecbd4432cf9fa8
unknown <pjaszek@gmail.com>
2021-09-22 21:52:19 +0200

    changed in Student Class

--- src/main/java/com/example/model/Student.java
+++ src/main/java/com/example/model/Student.java
@@ -1,6 +1,6 @@
 package com.example.model;
 
-
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Entity
@@ -24,12 +25,24 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 20, message
+            = "Imie musi zawierać od 1 do 2 liter")
     private String firstName;
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 20, message
+            = "Imie musi zawierać od 1 do 2 liter")
     private String secondName;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Pattern(regexp="(^[0-9]{10})")
     private int phoneNumber;
 
 
