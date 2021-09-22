6209257691ccaba6a99b4c0aaa0b4e6411e02079
unknown <pjaszek@gmail.com>
2021-09-22 20:28:15 +0200

    added student class

--- src/main/java/com/example/model/Student.java
+++ src/main/java/com/example/model/Student.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
@@ -16,9 +20,14 @@ import java.util.Date;
 @ToString
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String secondName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
     private String email;
     private int phoneNumber;
--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -1,4 +1,16 @@
 package com.example.service;
 
+import com.example.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
 }
